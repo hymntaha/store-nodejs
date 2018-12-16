b232119631203a3cadddc63cf3ced2e0175d7cbc
taha uygun <tahauygun@gmail.com>
2018-12-15 23:48:56 -0800

    adding middlewares

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express();
 
 app.use((req,res,next)=>{
     console.log('In the middleware');
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log('Another middleware');
+    next();
 });
 
 const server = http.createServer();
