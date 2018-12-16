e7b7257d117382512107ed2baf47f5f1920d9f2d
taha uygun <tahauygun@gmail.com>
2018-12-15 23:54:00 -0800

    express clean up

--- app.js
+++ app.js
@@ -3,13 +3,8 @@ const express = require('express');
 const app = express();
 
 app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next();
-});
-
-app.use((req,res,next)=>{
     console.log('Another middleware');
-    next();
+    res.send('<h1>Hello from express</h1>')
 });
 
 app.listen(3000);
