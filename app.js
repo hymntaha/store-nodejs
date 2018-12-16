f16aa72e87773084a282388436131fec47ceafb9
taha uygun <tahauygun@gmail.com>
2018-12-15 23:47:31 -0800

    package.json update

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 
 const app = express();
 
+app.use((req,res,next)=>{
+    console.log('In the middleware');
+});
+
 const server = http.createServer();
 
 server.listen(3000);
