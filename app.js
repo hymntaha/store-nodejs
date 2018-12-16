3303c2cc5da5a0d1d2acee65c3c0548186b36e40
taha uygun <tahauygun@gmail.com>
2018-12-15 23:40:08 -0800

    adding nodemon

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write('<body><h1>Hello from nodejs.</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+
 
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
