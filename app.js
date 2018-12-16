fd7a9a56c5243b56dc076930e2106aa4315185e5
taha uygun <tahauygun@tahas-MacBook-Pro.local>
2018-12-15 23:12:02 -0800

    setting header

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const http = require('http');
 
 const server = http.createServer((req,res) => {
     console.log(req.url, req.method, req.headers);
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
 });
 
 server.listen(3000);
