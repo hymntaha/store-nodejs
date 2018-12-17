666c7c21f4f84d17a4f355b20224f681210bfab7
taha uygun <tahauygun@gmail.com>
2018-12-16 20:03:12 -0800

    adding html to route via path

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,9 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req,res,next) => {
-    res.send(
-        '<form action="/product" method="post"><input type="text" name="title"><button type="submit">SUBMIT</button></form>'
-    );
+    res.sendFile(path.join(__dirname,'../', 'views','add-product.html'));
 });
 
 router.post('/product', (req,res,next) => {
