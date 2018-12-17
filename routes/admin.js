701f5cee9aacf46eb85419461bb94a2cd2b00ce7
taha uygun <tahauygun@gmail.com>
2018-12-16 20:09:29 -0800

    adding css

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,20 @@
 const path = require('path');
+
 const express = require('express');
-const router = express.Router();
+
 const rootDir = require('../util/path');
 
+const router = express.Router();
 
-router.get('/add-product', (req,res,next) => {
-    res.sendFile(path.join(rootDir, 'views','add-product.html'));
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/product', (req,res,next) => {
-    console.log(req.body);
-    res.redirect('/');
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 module.exports = router;
