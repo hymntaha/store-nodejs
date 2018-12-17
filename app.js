2fde64a9f0085fec72af0df68864705b54d1cf6c
taha uygun <tahauygun@gmail.com>
2018-12-16 19:32:08 -0800

    adding 404 page

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000);
