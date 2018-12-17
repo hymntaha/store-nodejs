48cedfca2fc0e6c659fb1d60b240339364d3bccc
taha uygun <tahauygun@gmail.com>
2018-12-16 17:40:35 -0800

    adding routes for shop

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+
 const bodyParser = require('body-parser');
 
 const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.use(bodyParser.urlencoded({extended: false}));
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/add-product', (req,res,next) => {
+router.get('/add-product', (req,res,next) => {
     res.send(
         '<form action="/product" method="post"><input type="text" name="title"><button type="submit">SUBMIT</button></form>'
     );
