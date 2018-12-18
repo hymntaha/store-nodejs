2d72b11bb4b814d7415989edbe30b13a651e8ec7
taha uygun <tahauygun@gmail.com>
2018-12-17 23:18:12 -0800

    refactor 404 page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -17,8 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
+
 
 app.listen(3000);
