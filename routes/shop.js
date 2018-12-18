23ca2f344e8cbe3b651522f44246dd7f03429655
taha uygun <tahauygun@gmail.com>
2018-12-17 23:03:02 -0800

    adding templating engine

--- routes/shop.js
+++ routes/shop.js
@@ -3,13 +3,20 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
-
-const router = express.Router();
 const adminData = require('./admin');
 
+const router = express.Router();
+
 router.get('/', (req, res, next) => {
-  console.log('shop.js',adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
