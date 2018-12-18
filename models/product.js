10739c50ecf2d0a5bd0af8b86554a45308c86ca4
taha uygun <tahauygun@gmail.com>
2018-12-17 23:40:00 -0800

    adding product saving functionality

--- models/product.js
+++ models/product.js
@@ -1,4 +1,6 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
 
 module.exports = class Product {
   constructor(t){
@@ -6,7 +8,18 @@ module.exports = class Product {
   }
 
   save(){
-    products.push(this);
+    const p = path.join(path.dirname(process.mainModule.filename),'data','products.json');
+    fs.readFile(p, (err,fileContent)=>{
+      let products = [];
+
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err)=>{
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
