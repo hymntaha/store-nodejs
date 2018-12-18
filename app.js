c49f49445a6624443a719673260ad1a198eca174
taha uygun <tahauygun@gmail.com>
2018-12-17 22:36:26 -0800

    adding products

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/admin',adminRoutes);
+app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
--- routes/shop.js
+++ routes/shop.js
@@ -5,8 +5,10 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
+  console.log('shop.js',adminData.products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
