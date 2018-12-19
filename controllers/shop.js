f4ebe603f0f9c3d764f2a9e55a4fcb0b3ea870fc
taha uygun <tahauygun@gmail.com>
2018-12-19 15:21:38 -0800

    detail product page

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,6 +12,16 @@ exports.getProducts =  (req, res, next) => {
 
 exports.getProduct =  (req, res, next) => {
   const prodId = req.params.productId;
+
+  Product.findById(prodId,product=> {
+    res.render('shop/product-detail',{
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  })
+
+  res.redirect('/');
 };
 
 exports.getIndex = (req,res,next) =>{
