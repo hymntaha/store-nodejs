246f52c380bda487ee072cdbc97306f93c6adb18
taha uygun <tahauygun@gmail.com>
2018-12-18 17:52:07 -0800

    adding some views for different routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     formsCSS: true,
@@ -16,15 +16,10 @@ exports.postAddProduct = (req, res, next) => {
   res.redirect('/');
 };
 
-exports.getProducts =  (req, res, next) => {
-  Product.fetchAll((products)=>{
-    res.render('shop', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
-    });
+exports.getProducts = (req,res,next) => {
+  res.render('admin/products', {
+    prods: products,
+    pageTitle: 'Admin Products',
+    path: '/admin/products'
   });
 };
