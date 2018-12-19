306487aee051e08aa23152cd730c8d6e8ee98774
taha uygun <tahauygun@gmail.com>
2018-12-18 18:50:52 -0800

    extracting dynamic params

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,10 @@ exports.getProducts =  (req, res, next) => {
   });
 };
 
+exports.getProduct =  (req, res, next) => {
+  const prodId = req.params.productId;
+};
+
 exports.getIndex = (req,res,next) =>{
   Product.fetchAll((products)=>{
     res.render('shop/index', {
