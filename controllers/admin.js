74151f4a96d7e4f92f2f3fdb6fa0abda3299c4d6
taha uygun <tahauygun@gmail.com>
2018-12-18 18:06:13 -0800

    product display bug fix

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,16 +15,17 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-
-  const product = new Product(title,imageUrl,description,price);
+  const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
 
-exports.getProducts = (req,res,next) => {
-  res.render('admin/products', {
-    prods: products,
-    pageTitle: 'Admin Products',
-    path: '/admin/products'
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
+    });
   });
 };
