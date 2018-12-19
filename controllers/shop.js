2a0a6994e99c257f0923f1e35b05e629074c3974
taha uygun <tahauygun@gmail.com>
2018-12-19 15:58:10 -0800

    add to cart work

--- controllers/shop.js
+++ controllers/shop.js
@@ -41,6 +41,11 @@ exports.getCart = (req, res, next) => {
   });
 }
 
+exports.postCart = (req, res, next) => {
+  const prodId  = req.body.productId;
+  res.redirect('/cart');
+}
+
 exports.getCheckout = (req,res,next ) =>{
   res.render('shop/checkout', {
     path: '/checkout',
