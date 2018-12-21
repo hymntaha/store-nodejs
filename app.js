5798a955b80b338259bf8cc415056c122ac5b50f
taha uygun <tahauygun@gmail.com>
2018-12-20 18:34:30 -0800

    using the database connection

--- app.js
+++ app.js
@@ -2,27 +2,36 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database');
 
+const errorController = require('../../Downloads/01-using-the-database-connection/controllers/error');
+const mongoConnect = require('../../Downloads/01-using-the-database-connection/util/database');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use((req, res, next) => {
+  // User.findById(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
+});
+
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect((client)=>{
+mongoConnect(client => {
   console.log(client);
   app.listen(3000);
-})
+});
