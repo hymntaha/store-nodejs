f4c6ed2acce4ba5d43c5fa34511508d53dc824e2
taha uygun <tahauygun@gmail.com>
2018-12-22 21:36:16 -0800

    adding the add to cart functionlity

--- app.js
+++ app.js
@@ -3,28 +3,28 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorController = require('../../Downloads/05-deleting-products/controllers/error');
-const mongoConnect = require('../../Downloads/05-deleting-products/util/database').mongoConnect;
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('../../Downloads/05-deleting-products/routes/admin');
-const shopRoutes = require('../../Downloads/05-deleting-products/routes/shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findById('5baa2528563f16379fc8a610')
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
