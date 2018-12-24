deb23049b490b7f7d6ef3f3c22478aa50948494d
taha uygun <tahauygun@gmail.com>
2018-12-23 20:03:37 -0800

    adding mongoose to ease workflow

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -32,6 +32,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect('mongodb+srv://tahauygun:TestUserForShopify@cluster0-v8atw.mongodb.net/test?retryWrites=true')
+  .then(result=>{
+    app.listen(3000);
+  })
+  .catch(err=> {
+    console.log(err);
+  });
