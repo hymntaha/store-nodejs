f97effeabcfaf4dd260fe6530251d975cab2eddc
taha uygun <tahauygun@gmail.com>
2018-12-25 17:04:44 -0800

    user modal set up

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('5baa2528563f16379fc8a610')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('5c22cfe8f7afd063b17a1108')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,9 +34,23 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://tahauygun:TestUserForShopify@cluster0-v8atw.mongodb.net/shop?retryWrites=true',
+    'mongodb+srv://tahauygun:tacoNy1985@cluster0-v8atw.mongodb.net/shop?retryWrites=true',
   )
   .then(result => {
+
+    User.findOne().then(user=>{
+      if (!user) {
+        const user = new User({
+          name: 'Taha',
+          email: 'tuygunny@gmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    })
+
     app.listen(3000);
   })
   .catch(err => {
