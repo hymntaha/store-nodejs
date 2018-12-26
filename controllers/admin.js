c7e6e3f629a6b146562cc2bfa095f43ea58fd2c7
taha uygun <tahauygun@gmail.com>
2018-12-25 16:26:08 -0800

    updating products

--- controllers/admin.js
+++ controllers/admin.js
@@ -77,7 +77,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
--- routes/shop.js
+++ routes/shop.js
@@ -8,9 +8,9 @@ const router = express.Router();
 
 router.get('/', shopController.getIndex);
 //
-// router.get('/products', shopController.getProducts);
+router.get('/products', shopController.getProducts);
 //
-// router.get('/products/:productId', shopController.getProduct);
+router.get('/products/:productId', shopController.getProduct);
 //
 // router.get('/cart', shopController.getCart);
 //
