265a965c42c2a3b56ceebcf2007bc594838b1038
taha uygun <tahauygun@gmail.com>
2018-12-27 23:42:40 -0800

    signup flow

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,6 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  //   const isLoggedIn = req
-  //     .get('Cookie')
-  //     .split(';')[1]
-  //     .trim()
-  //     .split('=')[1] === 'true';
-  console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -12,17 +8,31 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
-exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  req.session.user = user;
-  res.session.save((err)=>{
-    console.log(err);
-    res.redirect('/');
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
   });
 };
 
+exports.postLogin = (req, res, next) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {};
+
 exports.postLogout = (req, res, next) => {
-  req.session.destroy((err)=>{
+  req.session.destroy(err => {
     console.log(err);
     res.redirect('/');
   });
