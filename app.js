7c60b7f83102f4f249946b6f2b7d90f4f68198db
taha uygun <tahauygun@gmail.com>
2018-12-27 23:23:10 -0800

    make add cart work again

--- app.js
+++ app.js
@@ -36,6 +36,18 @@ app.use(
   })
 );
 
+app.use((req, res,next) => {
+  if(!req.session.user ){
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+        next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use((req, res, next) => {
   User.findById('5bab316ce0a7c75f783cb8a8')
     .then(user => {
