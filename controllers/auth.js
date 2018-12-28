bb6fe95e7b8f162715a61e1bd32fd3878346b0d9
taha uygun <tahauygun@gmail.com>
2018-12-27 22:56:02 -0800

    adding destroy session

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,5 +18,8 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy();
+  req.session.destroy((err)=>{
+    console.log(err);
+    res.redirect('/');
+  });
 };
