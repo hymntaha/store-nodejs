6ec2306ab4a127222b7ad807c2b912fdbd4e5726
taha uygun <tahauygun@gmail.com>
2018-12-31 00:00:01 -0800

    fix for app.js

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
