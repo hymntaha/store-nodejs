db1d13a24b38122b8ea2cca88b3c154996e03c03
taha uygun <tahauygun@gmail.com>
2018-12-30 23:59:01 -0800

    user route fix

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,6 @@ const express = require('express');
 const shopController = require('../controllers/shop');
 const isAuth = require('../middleware/is-auth');
 
-
 const router = express.Router();
 
 router.get('/', shopController.getIndex);
