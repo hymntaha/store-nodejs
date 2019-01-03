c4410598678c576498c960c57c4b7b2cfe6cbbd4
taha uygun <tahauygun@gmail.com>
2019-01-02 19:32:59 -0800

    adding async validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -77,47 +77,33 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
+
   const errors = validationResult(req);
   if (errors.isEmpty()) {
-    return res.status(422).render('auth/signup',{
-      path:'/signup',
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
       pageTitle: 'Signup',
-      errorMessage: errors.array()[0].message
+      errorMessage: errors.array()[0].message,
     });
   }
-
-  User.findOne({email: email})
-    .then(userDoc => {
-      if (userDoc) {
-        req.flash(
-          'error',
-          'E-Mail exists already, please pick a different one.',
-        );
-        return res.redirect('/signup');
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then(hashedPassword => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: {items: []},
-          });
-          return user.save();
-        })
-        .then(result => {
-          res.redirect('/login');
-          return transporter.sendMail({
-            to: email,
-            from: 'shop@node-complete.com',
-            subject: 'Signup succeeded!',
-            html: '<h1>You successfully signed up!</h1>',
-          });
-        })
-        .catch(err => {
-          console.log(err);
-        });
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] },
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+      return transporter.sendMail({
+        to: email,
+        from: 'shop@node-complete.com',
+        subject: 'Signup succeeded!',
+        html: '<h1>You successfully signed up!</h1>',
+      });
     })
     .catch(err => {
       console.log(err);
