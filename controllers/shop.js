ad2d93cb537a012bfddf3f00940da6d18e0c73ca
taha uygun <tahauygun@gmail.com>
2019-01-04 22:57:27 -0800

    generating .pdf files with order data

--- controllers/shop.js
+++ controllers/shop.js
@@ -143,7 +143,25 @@ exports.getInvoice = (req, res, next) => {
       pdfDoc.pipe(fs.createWriteStream(invoicePath));
       pdfDoc.pipe(res);
 
-      pdfDoc.text('Hello World!');
+      pdfDoc.fontSize(26).text('Invoice', {
+        underline: true,
+      });
+      pdfDoc.text('---------------------');
+      let totalPrice = 0;
+      order.products.forEach(prod => {
+        totalPrice = +prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(14)
+          .text(
+            prod.product.title +
+              ' - ' +
+              prod.quantity +
+              ' x ' +
+              prod.product.price,
+          );
+      });
+      pdfDoc.text('-----------------');
+      pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
       pdfDoc.end();
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
