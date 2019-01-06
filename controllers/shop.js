1b4fb2fa83a11cbaecc03fc0a66f932a39b30015
taha uygun <tahauygun@gmail.com>
2019-01-05 17:07:03 -0800

    retrieving a chunk of data

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,6 +3,8 @@ const Order = require('../models/order');
 const fs = require('fs');
 const path = require('path');
 const PDFDocument = require('pdfkit');
+const ITEMS_PER_PAGE = 2;
+
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -33,7 +35,12 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = req.query.page;
+
+
   Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
     .then(products => {
       res.render('shop/index', {
         prods: products,
